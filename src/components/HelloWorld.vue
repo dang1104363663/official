5f64c54289f6520386ea6dbf3bee3a4663ab5bfa
Daggo <50734937+dang1104363663@users.noreply.github.com>
2019-06-05 15:13:45 +0800

    为什么不能热保存了呢

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="hello">
     <div class="top_image"></div>
-    <div class="silderShow">
-      <el-carousel :interval="4000" type="card" height="20px" direction="vertical">
-        <el-carousel-item v-for="item in 6" :key="item">
-          <h3 class="medium">{{ item }}</h3>
-        </el-carousel-item>
-      </el-carousel>
-    </div>
+    <div class="silderShow"></div>
     <div class="LoverShow">
       <div class="Love_01">
         <div class="Lover_img"></div>
@@ -205,21 +199,6 @@
     border: 1px solid red;
     height: 600px;
     width: 220px;
-  }
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 10px;
-    opacity: 0.75;
-    line-height: 20px;
-    margin: 0;
-  }
-
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
   }
   .LoverShow {
     float: left;
