9b6edaf4cfcfcc10852c2134d0ac7d35317bca56
DESKTOP-0QLITDN\23382 <wc0923@outlook.com>
2019-06-08 15:52:47 +0800

    wc

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import $ from 'jquery'
+import 'lib-flexible'
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
